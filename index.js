dbecf4d5e510d1086195e897d05d146b57b177c7
Mario Mendes <mario.c.mendes1985@gmail.com>
2014-07-21 23:58:43 +0100

    erase all code and begin from scratch for index and tests

--- index.js
+++ index.js
@@ -2,205 +2,43 @@ var Hapi = require('hapi');
 var path = require('path');
 var Joi = require('joi');
 var cradle = require('cradle'); //we require the cradle framework that works with couchdb
-var db = new(cradle.Connection)().database('logs'); //set a connection to our database by passing the name of our databse as the only argument to
-                                                    //database(). Cradle takes care of the rest for us!
+var db = new(cradle.Connection)(); //set a connection to couchDb
 
-var server = Hapi.createServer('localhost', 8080, options);
+var server = Hapi.createServer('localhost', 8080);
 
-//set options for server
-var options = {
-  cache: {
-    engines: require('catbox-redis'),
-    options: {
-      host: 'localhost',
-      partition: 'LogIt',
-      password: 'password'
-    }
-  },
+logCont = { };
 
-  views:  {
-    path: path.join(__dirname + './lib/templates'),
-    engines: {
-      html: require('handlebars')
-    },
-    helpersPath: path.join(__dirname, 'happyhelper')
-  }
-};
-
-//Here we assign our function to getAllLogs for later use. This function will get our database and display it.
-
-
-var cache = server.cache('createdLogs', { expiresIn:60*60*1000 });
-
-var createdLogs = [
-  {
-    MuscleGroup: 'Arms',
-    'Date': '12-08-2014',
-    Exercise: 'Bicep curl'
-  },
-  {
-    MuscleGroup: 'Legs',
-    'Date': '11-08-214',
-    Exercise: 'Leg Press'
-    }
-];
-//create a place to store all our functions. This will make maintainability easier
-var logController = { };
-
-//This function will fetch specific logs according to the params id passed
-logController.allLogs = function (req , reply) {
-  var id = req.params.id; //assign params to the id variable. This will allow us to access our databse through the document id
-
-      db.get( '_all_docs' , function (err , doc){
-        //console.log(doc);
-          return reply(doc);//This simple function will display all the docs in our databse.
-      });
-};
-
-//This function will check the existence of a database
-/*logController.checkDb = {
+logCont.checkDb = {
+  //The first function will check for the existence of a database
   handler: function(req, reply) {
-
-    db.exists(function (err, exists) {
-      if (exists) {
-        console.log('Database ' + req.params.dbname + ' exists!' + " || " + statusCode());
-        return reply('Database ' + req.params.dbname + ' exists!').code(200);
-      } else {
-        console.log('Database ' + req.params.dbname + ' does not exist!' + " || " + statusCode());
-        return reply('Database ' + req.params.dbname + ' does not exist!').code(404);
+  db.exists(function (err, exists) {
+      if (!exists) {
+        console.log('The requested database does not exist.');
+        reply('The requested database does not exist.');
+        db.create(req.params.dbname);
+        return reply('New database ' + req.params.dbname + ' created!');
+      } else if(exists) {
+        console.log('Database ' + req.params.dbname + ' exists.');
+        return reply('Database ' + req.params.dbname + ' found!');
       }
     });
   },
+  //make sure that our databse name will be of the alphanumerical type
   validate: {
-      params: { id:Joi.string().alphanum()} // make sure that our requested dbname   is a string of the alphanumerical type with validation
+    params: { id:Joi.string().alphanum().min(4).max(15)}
   }
 };
-*/
-//This function gets the chosen log according to log id
-logController.chooseLogs = {
-  handler: function (req , reply) {
-  var id = req.params.id;
-  console.dir(req.params);
-
-
-    db.head(id , function (err , opt1, opt2 ) {//We use the db.head function to determine if there are any docs available in our database
-      if(opt1 !== '404' && id!=='undefined') { //First we check to see if there is a doc by writing opt1 !== '404' and id!=='undefined'
-        db.get(id , function (err, doc) {      //If these two condition are not met, we return an error 404, with the message 'document no found'
-           if(err) {
-             console.log ( 'Error: ' + err + " || " + 'No document found!'  );
-             return reply( Hapi.error.notFound ('Document not found!'));
-             } else {
-               // console.log(doc);
-                 return reply(doc); //If the conditions above are met, then we return the required document.
-             }
-         });
-       }
-   });
- },
-};
-
-//This fucntion will create a newLog and then add it with the push method to
-//createdLogs. We also validate with Joi that the input data is of the correct type
-logController.addLogs = {
-  handler: function(req, reply) {
-
-  var date = new Date().toString(); //set the date to be automatic and return the current date in a readable string
-
-  var newLog = {
- // this will be the payload
-        Date: date,
-        Exercise: req.payload.Exercise,
-        MuscleGroup: req.payload.MuscleGroup,
-        Set1: req.payload.Set1,
-        Set2: req.payload.Set2,
-        Set3: req.payload.Set3
-  };
-
-    db.save(newLog, function(err, res) {
-      if(err){
-        console.log("Error: " + err + ' || ' + 'Status Code: ' + statusCode());
-        return res('Something went wrong when saving the document! ' + ' Status Code: ' + statusCode());
-      } else {
-        console.log('The document was created with sucess!' + statusCode());
-        return res('Document created and saved with succes!');
-      }
-      http_res.end(response);
-    }); //add the newly created log to our list of already existing logs
-   //display the newly created log
-
-  }
-};
-
-//This function will delete a specified log from createdLogs with the slice method
-//If the selected log is not found, it will display a message informing that there is no log.
-logController.deleteLog = {
-  handler: function(req, reply) {
-    var id = Number(req.params.id);
-    if ( id >= createdLogs.length || typeof id === 'undefined') { //we will display a 404 message if id is greater than our logs
-                                                                  // length(can't delete what doesn't exist) OR if it is undefined
-      console.log("------->>> delete req.params.id: " + req.params.id + " | id: " + id);
-      return reply('No Logs found!').code(404);
-    }
-      createdLogs.splice(id, 1); //if the above condition is false, then we will delete the selected log from our list
-      console.log('--------> New creadtedLogs: ' + createdLogs.toString());
-      return reply(true);
-    },
-    validate: {
-        params: { id:Joi.string().alphanum()} // make sure that our requested id is a string of the alphanumerical type with validation
-    }
-  };
 
 
 var routes = [
-//set and store routes in array for code readability
 
-//get path to styles folder
-  /*{path: '/styles/{filename*}', method: 'GET', handler: {
-    directory: {
-      path: path.join(__dirname + './lib/styles')//join current directory root folder with path to styles folder to get styles
-      }
-    }
-  },
+//Route to custom database
+  {path: '/{req.params.dbname}/', method: 'GET', config: logCont.checkDb}
 
-//get path to images folder
-  {path: '/public/{filename*}', method: 'GET', handler: {
-    directory: {
-      path: path.join(__dirname + './lib/public')
-      }
-    }
-  },
 
-//get path to scripts folder
-  {path: '/scripts/{filename*}', method: 'GET', hanlder: {
-    directory: {
-      path: path.join(__dirname + './lib/scripts')
-      }
-    }
-  }
-*/
-//set routes to html files
-
-//set home.html as main page
-  {path: '/', method: 'GET', handler: {file: './lib/html/home.html'}},
-//List of databses
-//  {path: '/{dbname}', method: 'GET', config: logController.checkDb},
-//get all Logs
-  {path: '/yourlogs/', method: 'GET', handler: logController.allLogs},
-//choose logs from databse
-  {path: '/yourlogs/{id}', method: 'GET', config: logController.chooseLogs},
-//set path to add logs
-  {path: '/yourlogs/newlog', method: 'POST', config: logController.addLogs},
-//set the path to delete logs
-  {path: '/yourlogs/deletelog/{id}', method: 'DELETE', config: logController.deleteLog}
 ];
 
-server.route(routes);
 
-if(!module.parent){
-server.start(function(){
-  console.log('Go to localhost:8080/');
-});
-}
 
-server.createdLogs = createdLogs;//export creastedLogs to the test.js file in the test folder
+
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -3,141 +3,20 @@ var server = require('../'); //require index.js
 //write individual tests for your functions with Lab.experiment
 
 //name our test units
-Lab.experiment("Basic All Log Tests", function(){
+Lab.experiment("LogIt Tests", function(){
     //define the tests
-    Lab.test("List all logs at url /yourlogs/", function(done){
+    Lab.test("List all logs at url /Logs", function(done){
       var options = {
         method: 'GET',
-        url: '/yourlogs/'
+        url: '/Logs/'
       };
       //server.inject lets you simulate an http request
+      //For our first test we do not have any documents in the databse.
       server.inject(options, function(response) {
-        Lab.expect(response.statusCode).to.equal(200);//Expect http response status code to be 200('OK')
-        Lab.expect(response.result).to.be.an('array');
+        Lab.expect(response.statusCode).to.equal(404);//Expect http response status code to be 404('Not Found')
       done();
       });
     });
-
-  //define tests
-  Lab.test("List all logs at url /yourlogs/ - Error", function(done){
-    var options = {
-      method: 'GET',
-      url: '/yourlogs'
-    };
-    //server.inject lets you simulate an http request
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(404);//Expect http response status code to be 400
-    done();
-    });
-  });
-
-
-  //define logs by id
-  Lab.test('List Logs by id', function(done){
-    var options = {
-      method: 'GET',
-      url: '/yourlogs/Back'
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(200);
-    done();
-    });
-  });
-
-  Lab.test('List Logs by id - Error', function(done){
-    var options = {
-      method: 'GET',
-      url: '/yourlogs/foozball'
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(404);
-      Lab.expect(response.result.message).to.equal('Document not found!');
-    done();
-    });
-  });
-
-
-//tests to verify existence of databse
-/*    Lab.test('Existence of databse', function(done){
-    var options = {
-      method: 'GET',
-      url: '/logs/'
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(200);
-    done();
-    });
-  });
-
-  Lab.test('Existence of databse - Error', function(done){
-    var options = {
-      method: 'GET',
-      url: '/bumbigana/'
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(404);
-      Lab.expect(response.result.message).to.equal('Database bumbigana does not exist!');
-    done();
-    });
-  });
-*/
-
-//tests for creation of new docs in db
-  Lab.test('Create new log', function(done){
-    var date = new Date().toString();
-    var options = {
-      method: 'POST',
-      url: '/yourlogs/newlog',
-      payload: {
-        MuscleGroup: 'Chest',
-        'Date': date,
-        Exercise: "Inclined Press",
-        Set1: {Reps:12, Kg:99},
-        Set2: {Reps:42, Kg:21},
-        Set3: {Reps:22, Kg:33}
-      }
-    };
-
-    server.inject(options, function(response) {
-      //Lab.expect(response.statusCode).to.equal(201);
-      Lab.expect(response.result).to.equal({"ok": true});
-      Lab.expect(response.result).to.not.equal({MuscleGroup: 'Chest','Date': '22-11-2222'});
-    done();
-    });
-  });
-
-
-//tests for deletion of docs
-  Lab.test('Delete log', function(done){
-    var options = {
-      method: 'DELETE',
-      url: '/yourlogs/deletelog/1',
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(200);
-      Lab.expect(response.result).to.deep.equal(true);
-      Lab.expect(response.result).to.not.equal({MuscleGroup: 'Arms','Date': '12-08-2014',Exercise: 'Bicep curl'},{MuscleGroup: 'Legs','Date': '11-08-214',Exercise: 'Leg Press'});
-    done();
-    });
-  });
-
-  Lab.test('Delete log', function(done){
-    var options = {
-      method: 'DELETE',
-      url: '/yourlogs/deletelog/6',
-    };
-
-    server.inject(options, function(response) {
-      Lab.expect(response.statusCode).to.equal(404);
-      Lab.expect(response.result).to.equal('No Logs found!');
-    done();
-    });
-  });
 
 
 });
