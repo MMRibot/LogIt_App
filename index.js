6d5dd1e4d55fef9992afc160a55cf06b0bd2acfe
Mario Mendes <mario.c.mendes1985@gmail.com>
2014-07-22 22:58:50 +0100

    update index and test files. Remove cradle from dependicies and dump chouchDb. Change to chouchbase

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Hapi = require('hapi');
 var path = require('path');
 var Joi = require('joi');
-var couchbase = require('couchbase').Mock;
-var db = new couchbase.Connection();
+//Connect to database.
+var couchbase = require('couchbase');
+var cluster = new couchbase.Cluster();
+var db = db || cluster.openBucket('default');//check to see if we have a connection. If there is none, created one!
 
 
 var server = Hapi.createServer('localhost', 8000);
@@ -12,13 +14,10 @@ var logCont = { };
 logCont.checkDb = {
   //The first function will check for the existence of a database
   handler: function(req, reply) {
-      bucket.get('default', function(err, result){
-        if(err) {
-          console.log(err);
-          console.dir(err);
-        } else {
+      var bucketName = req.params.dbname;
+      db.get(bucketName, function(err, result){
+        console.log(result);
         return result;
-        }
       });
       return reply(result);
     }
